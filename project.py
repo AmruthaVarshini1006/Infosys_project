b440343b05a766e5138d1dae69ff4d48c390aa10
AmruthaVarshini1006 <goneamruthavarshini@gmail.com>
2026-01-29 16:58:07 +0530

    Update project.py

--- project.py
+++ project.py
@@ -37,7 +37,7 @@ gemini_llm = ChatGoogleGenerativeAI(
 )
 
 groq_llm = ChatGroq(
-    model="llama3-8b-8192",
+    model="llama-3.3-70b-versatile",
     temperature=0,
     api_key=st.secrets["GROQ_API_KEY"]
 )
